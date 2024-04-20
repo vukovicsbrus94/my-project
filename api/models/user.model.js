6ff70db859aa85c0855d141f1e5757f8056303b5
Stefan Vukovic <ultraboys994@gmail.com>
2024-04-20 02:12:24 +0200

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,21 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://thumbs.dreamstime.com/b/businessman-avatar-image-beard-hairstyle-male-profile-vector-illustration-178545831.jpg",
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "booking-3c053.firebaseapp.com",
+  projectId: "booking-3c053",
+  storageBucket: "booking-3c053.appspot.com",
+  messagingSenderId: "536989775479",
+  appId: "1:536989775479:web:3bae06b353784287b93872",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
