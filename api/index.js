a98a828b034f9e458225cfb6ef9db12d3de12b8a
Stefan Vukovic <ultraboys994@gmail.com>
2024-04-29 01:56:38 +0200

    update render

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+    const __dirname = path.resolve();
+
 
 const app = express();
 
@@ -34,11 +35,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
