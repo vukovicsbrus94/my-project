2915dd8730af7b7eb9b711bf75fcb5aa7dc926ed
Stefan Vukovic <ultraboys994@gmail.com>
2024-04-29 02:35:11 +0200

    1

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -16,7 +16,7 @@ import {
 } from "react-icons/fa";
 import Contact from "../components/Contact";
 
-
+// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -34,7 +34,6 @@ export default function Listing() {
         setLoading(true);
         const res = await fetch(`/api/listing/get/${params.listingId}`);
         const data = await res.json();
-        console.log(data);
         if (data.success === false) {
           setError(true);
           setLoading(false);
@@ -137,7 +136,7 @@ export default function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
-            {currentUser && (
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
                 onClick={() => setContact(true)}
                 className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
